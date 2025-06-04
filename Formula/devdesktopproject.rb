b0b57e1ac9ab5949b53cd3771c69ec9bd75bd396
Bisounours <yan.dai@outlook.fr>
2025-06-05 00:40:02 +0200

    Mise à jour de l'URL de téléchargement dans devdesktopproject.rb pour refléter le nouveau nom de fichier "Human.Agement-1.0.0-arm64.dmg".

--- Formula/devdesktopproject.rb
+++ Formula/devdesktopproject.rb
@@ -1,7 +1,7 @@
 class Devdesktopproject < Formula
   desc "Mon projet Electron + Vite + React"
   homepage "https://github.com/bisounours1111/dev-desktop-project"
-  url "https://github.com/bisounours1111/dev-desktop-project/releases/download/v1/DevDesktopProject-1.0.0.dmg"
+  url "https://github.com/bisounours1111/dev-desktop-project/releases/download/v1/Human.Agement-1.0.0-arm64.dmg"
   sha256 "5032da418849b99de7ffa197c1065536551a7bdb95982c974b882ff9c13c88da"
 
   def install
