99c22fcf5c9fffde6ab2f321d666b945653afd17
Bisounours <yan.dai@outlook.fr>
2025-06-05 00:21:13 +0200

    add

---
+++ Formula/devdesktopproject.rb
@@ -0,0 +1,21 @@
+class DevDesktopProject < Formula
+    desc "Mon projet Electron + Vite + React"
+    homepage "https://github.com/bisounours1111/dev-desktop-project"
+    url "https://github.com/bisounours1111/dev-desktop-project/releases/download/v1/DevDesktopProject-1.0.0.dmg"
+    sha256 "5032da418849b99de7ffa197c1065536551a7bdb95982c974b882ff9c13c88da"
+  
+    def install
+      system "hdiutil", "attach", cached_download
+      system "cp", "-r", "/Volumes/DevDesktopProject/DevDesktopProject.app", prefix
+      system "hdiutil", "detach", "/Volumes/DevDesktopProject"
+    end
+  
+    def caveats
+      <<~EOS
+        DevDesktopProject est installé dans #{prefix}/DevDesktopProject.app.
+        Tu peux lancer l'application avec :
+          open #{prefix}/DevDesktopProject.app
+      EOS
+    end
+  end
+  
