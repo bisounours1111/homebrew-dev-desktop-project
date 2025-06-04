41c0bc5833f9898f574709cecb6f681e58a65095
Bisounours <yan.dai@outlook.fr>
2025-06-05 00:51:09 +0200

    Mise à jour du hash SHA256 dans devdesktopproject.rb pour la version "1.0.0".

--- Formula/devdesktopproject.rb
+++ Formula/devdesktopproject.rb
@@ -3,7 +3,7 @@ class Devdesktopproject < Formula
   homepage "https://github.com/bisounours1111/dev-desktop-project"
   url "https://github.com/bisounours1111/dev-desktop-project/releases/download/v1/Human-Agement.dmg"
   version "1.0.0"
-  sha256 "b90815f644c8dab3066f7f085349c8b1c4163e098b1a9074ddfa666c06b8b854"
+  sha256 "64a92a50d61f528030743a3a7f6e4b4c8f331d277bcd61089e69a29fdce2a215"
 
   def install
     system "hdiutil", "attach", cached_download
