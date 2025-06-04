79fa3336d18b121101fe2f53d2ad5266e235d96b
Bisounours <yan.dai@outlook.fr>
2025-06-05 00:50:17 +0200

    Ajout de la version "1.0.0" dans devdesktopproject.rb pour le projet Electron + Vite + React.

--- Formula/devdesktopproject.rb
+++ Formula/devdesktopproject.rb
@@ -2,6 +2,7 @@ class Devdesktopproject < Formula
   desc "Mon projet Electron + Vite + React"
   homepage "https://github.com/bisounours1111/dev-desktop-project"
   url "https://github.com/bisounours1111/dev-desktop-project/releases/download/v1/Human-Agement.dmg"
+  version "1.0.0"
   sha256 "b90815f644c8dab3066f7f085349c8b1c4163e098b1a9074ddfa666c06b8b854"
 
   def install
